6d1dfe59728dbf396f3725b635f48d6776f39545
reincas <r.caspary@posteo.de>
2024-02-08 17:19:31 +0100

    Extract dhmclient

--- DhmClient/holoclient/__init__.py
+++
@@ -1,9 +0,0 @@
-##########################################################################
-# Copyright (c) 2024 Reinhard Caspary                                    #
-# <reinhard.caspary@phoenixd.uni-hannover.de>                            #
-# This program is free software under the terms of the MIT license.      #
-##########################################################################
-
-from .holoclient import HoloClient
-from .rawholo import HoloContainer
-from .dhmclient import DhmClient
